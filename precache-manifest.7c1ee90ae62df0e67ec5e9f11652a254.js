d0b16ac1fc0951f9a2cf8f409dcaf99c522a48e8
dfifer <dfifer@purdue.edu>
2021-05-28 21:10:35 -0400

    Updates

---
+++ precache-manifest.7c1ee90ae62df0e67ec5e9f11652a254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317ae700f73a5b8e9acfd039d10f7ffe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e7cd4bad1a4f54aabd6",
+    "url": "./static/css/main.bc28789c.chunk.css"
+  },
+  {
+    "revision": "cb95b1fa4d996614da3f",
+    "url": "./static/js/2.573e8159.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.573e8159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7cd4bad1a4f54aabd6",
+    "url": "./static/js/main.6bb05f5c.chunk.js"
+  },
+  {
+    "revision": "9ae710465fe226568fdd",
+    "url": "./static/js/runtime-main.74bdcf18.js"
+  }
+]);
